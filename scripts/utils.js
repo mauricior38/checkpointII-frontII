8970609fd5eb3b1552aef3183a79579248931f5b
Mauricio <r38.mauricio@gmail.com>
2022-12-01 00:10:01 -0300

    [FIXED] Looping de verificação

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,19 +1,3 @@
 function baseUrl() {
   return "https://ctd-fe2-todo-v2.herokuapp.com/v1";
 }
-
-function verificaLogin() {
-  let isLogged = localStorage.getItem("todoToken");
-
-  let body = document.querySelector("body")
-
-  if(isLogged){
-    
-  }else{
-    console.log("false")
-  }
-
-//   {isLogged ? window.location.href = ("./tarefas.html") : window.location.href = ("./index.html")}  
-  
-
-}
